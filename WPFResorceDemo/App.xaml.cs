75f5d0424be29041d591a2dac6fc144566756872
Сергей Лакас <serhiy.lakas@hotmail.com>
2021-03-30 18:14:03 +0300

    add resourse

---
+++ WPFResorceDemo/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WPFResorceDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnLoadCompleted(NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+            this.Resources.MergedDictionaries.Clear();
+            var r = new ResourceDictionary();
+            r.Source = new Uri("WhiteThemeDictionary.xaml");
+            this.Resources.MergedDictionaries.Add(r);
+        }
+    }
+}
